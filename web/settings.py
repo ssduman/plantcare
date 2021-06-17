2c97fc2f2b8f24bcd9c784e270879c5deb5f54f6
ssduman <sduman1818@gmail.com>
2021-06-17 17:31:29 +0300

    hide secret key

--- web/settings.py
+++ web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_2u(bqj12^xag)c^rg#tpx(%q4m(fej--p%k6)#$b(bgosdeb-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
